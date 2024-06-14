ea4628845e1f11e26a7888c17e9f33a3d23cb3db
Spektre DeFi <162076912+SpektreDeFi@users.noreply.github.com>
2024-06-14 16:17:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.getElementById('nftForm').addEventListener('submit', async function(event) {
   event.preventDefault();
   
-  const tokenAddress = document.getElementById('tokenAddress').value;
+  const groupKey = 'collection';
+  const groupValue = document.getElementById('groupValue').value;
   const resultsDiv = document.getElementById('results');
-  resultsDiv.innerHTML = 'Fetching metadata...';
+  resultsDiv.innerHTML = 'Fetching assets...';
 
   try {
-    const response = await fetch(`/api/nftMetadata?tokenAddress=${tokenAddress}`);
+    const response = await fetch(`/api/nftMetadata?groupKey=${groupKey}&groupValue=${groupValue}`);
     console.log('Network response status:', response.status); // Log status
     if (!response.ok) {
       const errorText = await response.text(); // Get error text for more details
       throw new Error(`Network response was not ok: ${errorText}`);
     }
-    const metadata = await response.json();
-    resultsDiv.innerHTML = JSON.stringify(metadata, null, 2);
+    const assets = await response.json();
+    resultsDiv.innerHTML = JSON.stringify(assets, null, 2);
   } catch (error) {
-    resultsDiv.innerHTML = 'Error fetching metadata: ' + error.message;
+    resultsDiv.innerHTML = 'Error fetching assets: ' + error.message;
   }
 });
