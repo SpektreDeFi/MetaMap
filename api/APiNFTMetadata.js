b8e1802f1b9b27fc422e4ccdf4180c0061be1870
Spektre DeFi <162076912+SpektreDeFi@users.noreply.github.com>
2024-06-14 17:02:00 -0700

    Update APiNFTMetadata.js

--- api/APiNFTMetadata.js
+++ api/APiNFTMetadata.js
@@ -4,17 +4,24 @@ import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
 export default async (req, res) => {
   const { groupKey, groupValue } = req.query;
 
-  console.log(`Received request with groupKey: ${groupKey}, groupValue: ${groupValue}`);
-
   try {
     const umi = createUmi('https://rpc.helius.xyz').use(dasApi());
 
-    const assets = await umi.rpc.getAssetsByGroup({ groupKey, groupValue });
-    console.log('Fetched assets:', assets);
+    const assets = await umi.rpc.getAssetsByGroup({
+      groupKey,
+      groupValue
+    });
 
+    console.log('Fetched assets:', assets);
     res.json(assets);
   } catch (error) {
     console.error('Error fetching assets:', error.message || error);
+
+    // Log the full response for debugging
+    if (error.response) {
+      console.error('Response:', error.response);
+    }
+    
     res.status(500).json({ error: 'Server Error', details: error.message || error });
   }
 };
